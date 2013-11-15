c2e79c92edba823a4ee831209f5c2e7f8892e97b
Ryan Vergeront <ryan.vergeront@gmail.com>
2013-11-15 16:57:41 -0500

    Add EventStreaming#stream_messages

--- lib/zulip/client/event_streaming.rb
+++ lib/zulip/client/event_streaming.rb
@@ -1,6 +1,30 @@
 module Zulip
   module EventStreaming
 
+    def stream_messages(&block)
+      stream_events("message", &block)
+    end
+
+    def stream_events(*event_types, &block)
+      event_types.flatten!
+
+      queue = register(event_types)
+      queue_id = queue.queue_id
+      last_event_id = queue.last_event_id
+
+      loop do
+        events = get_events(queue_id, last_event_id)
+        last_event_id = max_event_id_from(events)
+
+        events.each do |event|
+          yield Zulip::EventParser.parse(event) if event_types.include?(event["type"])
+        end
+
+      end
+    end
+
+
+    # Makes a longpulling request on an event queue
     # Accepts an object that responds to #queue_id && #last_event_id
     # Alternativly takes a queue id and a last event id
     def get_events(registered_queue_or_queue_id, last_event_id=nil)
@@ -26,6 +50,10 @@ module Zulip
     def event_response
       response = connection.get("/v1/events").body
       JSON.parse(response)
+    end
+
+    def max_event_id_from(events)
+      events.map{ |event| event['id']}.max
     end
   end
 end
